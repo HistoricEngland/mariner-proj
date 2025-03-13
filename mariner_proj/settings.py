9f916f7687a441c5d8c130be3cffe0f72b36eea0
Edward Dening <edward.dening@historicengland.org.uk>
2025-03-13 16:34:58 +0000

    Change preferred coordiante system to lat long

--- mariner_proj/settings.py
+++ mariner_proj/settings.py
@@ -257,13 +257,13 @@ PREFERRED_COORDINATE_SYSTEMS = (
         "name": "BNG",
         "srid": "27700",
         "proj4": "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +datum=OSGB36 +units=m +no_defs",
-        "default": True,
+        "default": False,
     },
     {
         "name": "LatLong",
         "srid": "4326",
         "proj4": "+proj=longlat +datum=WGS84 +no_defs",
-        "default": False,
+        "default": True,
     },  # Required
 )
 
