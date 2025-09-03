3ee79181ad11420e525f77b753791f64c59b107b
Andy Jones <andrew.jones@historicengland.org.uk>
2025-09-03 09:28:39 +0100

    Enhance documentation for BasePatch class with usage examples and clarification on patch application logic AB#93480

--- mariner_proj/utils/patches/base.py
+++ mariner_proj/utils/patches/base.py
@@ -1,13 +1,23 @@
 class BasePatch:
     """
+
     Base class for patches that can be applied to multiple classes.
     examples usage:
     ```
+    from arches.app.some.module import target_class_or_obj
+
+    def patch_target_function_or_object():
+        # define patch logic here
+        pass
+
     class MyPatch(BasePatch):
         def apply(self):
-            # apply patch to self.target_class
-            pass
+            # apply patch
+            target_class_or_obj.some_method = patched_some_method
     ```
+
+    Add patch modules to the /utils/patches directory
+
     """
 
     def apply(self):
