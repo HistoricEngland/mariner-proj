98333b25661dfa1efb67429a4a9a9e63a9bacbef
Rob Oates <rob.oates@historicengland.org.uk>
2025-10-08 10:39:37 +0100

    Set default backend cache to Redis, to prevent GitHub builds from failing

--- mariner_proj/settings.py
+++ mariner_proj/settings.py
@@ -327,7 +327,11 @@ PREFERRED_COORDINATE_SYSTEMS = (
 # For more info on configuring your cache: https://docs.djangoproject.com/en/2.2/topics/cache/
 CACHES = {
     "default": {
-        "BACKEND": "django.core.cache.backends.dummy.DummyCache",
+        # Setting to something other than DummyCache so that GitHub builds don't fail
+        # See check_cache_backend() in apps.py for other options
+        # Developing locally, you should use DummyCache and comment out RedisCache
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        # "BACKEND": "django.core.cache.backends.dummy.DummyCache",
     },
     "user_permission": {
         "BACKEND": "django.core.cache.backends.db.DatabaseCache",
