feefd529ce1228af9e06c1b77e58d8090a0b9bee
Andy Jones <andrew.jones@historicengland.org.uk>
2025-10-10 15:35:34 +0100

    silence checks in settings.py
    - handled in deploy files.
    - breaks checks

--- mariner_proj/settings.py
+++ mariner_proj/settings.py
@@ -522,6 +522,13 @@ SHOW_LANGUAGE_SWITCH = len(LANGUAGES) > 1
 # See tests.views.search_tests.TestEsMappingModifier class for example
 # ES_MAPPING_MODIFIER_CLASSES = ["mariner_proj.search.es_mapping_modifier.EsMappingModifier"]
 
+# Silence check that we havae made a decision on
+SILENCED_SYSTEM_CHECKS.append(
+    "arches.W001",  # Cache backend does not support rate-limiting - we set this in the deploy config
+    "arches.E001",  # Dummy Cache in production check - we set this in the deploy config
+)
+
+
 try:
     from .package_settings import *
 except ImportError:
